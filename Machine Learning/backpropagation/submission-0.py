a2e13fe77a85cf400538545be8f84cecabd09049
leonardosartori-web <79639983+leonardosartori-web@users.noreply.github.com>
2026-04-14 23:16:06 +0200

    Add: backpropagation - submission-0

---
+++ Machine Learning/backpropagation/submission-0.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import Tuple
+
+class Solution:
+    def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
+        
+        forward = x @ w + b
+        y_hat = 1 / (1+np.exp(-forward))
+
+        error = y_hat - y_true
+        delta = error * y_hat * (1-y_hat)
+        dL_dw = np.round(delta*x, 5)
+        dL_db = round(float(delta), 5)
+
+        return (dL_dw, dL_db)
+
+
