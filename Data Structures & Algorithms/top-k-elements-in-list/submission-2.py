18e2e70cbc304b2fe8b9775cd016b2dd637418de
leonardosartori-web <79639983+leonardosartori-web@users.noreply.github.com>
2026-05-11 22:36:00 +0200

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Idea: store occs of nums. In Freq[i] we have the num with i frequency.
+        # 
+        nums_occ = defaultdict(int)
+        freq = [[] for _ in range(len(nums)+1)]
+
+        for e in nums:
+            if e not in nums_occ:
+                nums_occ[e] = 0
+            nums_occ[e] += 1
+        
+        for e, cnt in nums_occ.items():
+            freq[cnt].append(e)
+        
+        res = []
+        i = len(freq) - 1
+        while k >= 0 and i >= 0:
+            for e in freq[i]:
+                if k > 0:
+                    res.append(e)
+                    k -= 1
+            i -= 1
+        return res
